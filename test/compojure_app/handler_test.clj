6a01696e576ce5c325d9fbfb1c79043382266a83
Alexey <alexey@circleci.com>
2017-07-18 10:33:37 +0200

    Fix typos in config and fix tests

--- test/compojure_app/handler_test.clj
+++ test/compojure_app/handler_test.clj
@@ -1,5 +1,6 @@
 (ns compojure-app.handler-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [ring.mock.request :as mock]
             [compojure-app.handler :refer :all]))
 
@@ -7,7 +8,7 @@
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (str/starts-with? (:body response) "Hello World"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
